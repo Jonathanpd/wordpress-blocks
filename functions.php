bff5b39c86f3c6de8cbc3cdd3b3c2b6f6caaa171
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2023-10-12 19:22:54 -0300

    feat: news templates block

--- functions.php
+++ functions.php
@@ -10,4 +10,4 @@ include(get_theme_file_path('/includes/setup.php'));
 // Hooks
 add_action('wp_enqueue_scripts', 'u_enqueue');
 add_action('wp_head', 'u_head', 5);
-add_action( 'after_setup_theme', 'u_setup_theme' );
+add_action('after_setup_theme', 'u_setup_theme');
